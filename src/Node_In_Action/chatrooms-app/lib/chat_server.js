41cbf3dbae6b924b94a010f7a4003e9b69c16a49
pit-dev <possibilitiesInTest@protonmail.com>
2019-12-27 01:36:01 -0500

    assignGuestName(), joinRoom():
    chat_server.js
         assignGuestName at connection
         -generate new guest name
         -associate connection id with new name
         -let user know their guestName
         -record the name
         -increment guestNumber count
    
         joinRom()
         -join room & set currentRoom
         -emit to user of joinResult {rm}
         -notify user of room entered
         -notify room of new user
         -define usersInRoom via clients()
         -list users in usersInRoomSummary
         -emit roomSummary to new user

--- src/Node_In_Action/chatrooms-app/lib/chat_server.js
+++ src/Node_In_Action/chatrooms-app/lib/chat_server.js
@@ -4,8 +4,8 @@ var nickNames = {};
 var namesUsed = [];
 var currentRoom = {};
 
-// is invoked in server.js
-exports.listen = function(server);
+// invoked in server.js
+exports.listen = function(server) {
 
     // starts SIO server on same port as httpServer
     io = socketio.listen(server);
@@ -20,7 +20,7 @@ exports.listen = function(server);
         // places user in lobby when they connect
         joinRoom(socket, 'Lobby');
 
-        // handle user messages, namechanges, room connects
+        // handles user messages, namechanges, room connects
         handleMessageBroadcasting(socket, nickNames);
         handleNameChangeAttempts(socket, nickNames, namesUsed)
         handleRoomJoining(socket);
@@ -30,9 +30,59 @@ exports.listen = function(server);
             socket.emit('rooms', io.sockets.manager.rooms);
         });
 
-        // define cleanup logic on disconnection
+        // defines clean up logic on disconnection
         handleClientDisconnection(socket, nickNames, namesUsed);
         });
 };
 
+// assigns guest name at connection
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+    // generates new guest name
+    var name = "Guest" + guestNumber;
+    // associates connection id with guestName
+    nickNames[socket.id] = name;
+    //let user know their guestName
+    socket.emit('nameResult', {
+        success: true,
+        name: name,
+    });
+    // note that guest name is now used
+    namesUsed.push(name);
+    //increment counter used to generate guest names
+    return guestNumber + 1;
+}
+
+// joins room
+function joinRoom(socket, room) {
+    // joins room
+    socket.join(room);
+    // sets currentRoom
+    currentRoom(socket.io) = room;
+    // lets user know they have entered
+    socket.emit('joinResult', {room: room});
+
+    // notifies room users of new entry
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + 'has joined' + room + '.'
+    });
+}
+
+// define users in same room as user
+var usersInRoom = io.sockets.clients(room);
+
+if (usersInRoom.length > 1) {
+    // lists users in room, upon new entry
+    var usersInRoomSummary = 'Users currently in ' + room + ': ';
+    for (var index in usersInRoom) {
+        if (userSocketId != socket.id) {
+            if(index > 0) {
+                usersInRoomSummary += ', ';
+            }
+            usersInRoomSummary += nickNames[userSocket.Id];
+        }
+    }
+    usersInRoomSummary += '.';
+    // emits users in room to new user
+    socket.emit('message', {text: usersInRoomSummary});
+}
 
