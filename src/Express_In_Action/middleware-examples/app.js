cf1b5851e205b914a5384ec047e2ab3f136b4dbc
pit-dev <possibilitiesInTest@protonmail.com>
2020-01-18 11:10:46 -0500

    middleware examples with morgan logger and resolving pathnames

--- src/Express_In_Action/middleware-examples/app.js
+++ src/Express_In_Action/middleware-examples/app.js
@@ -1,15 +1,27 @@
 var express = require("express");
+var logger = require("morgan");
 var http = require("http");
+
+// connect-ratelimite
+// Helmet
+// cookie-parser
+// response-time
 
 // creates express aplication
 var app = express();
 
 // middleware that provides logging
-app.use(function(request, response, next) {
-    console.log("In comes a request to: " + request.method + " to " + request.url);
-    next();
-    // response.end("Hello world");
-});
+app.use(logger("short"));
+// app.use(function(request, response, next) {
+//     console.log("In comes a request to: " + request.method + " to " + request.url);
+//     next();
+//     // response.end("Hello world");
+// });
+
+// sets up public path using Node's path module
+var publicPath = path.resolve(__dirname, "public");
+// sends static files from the publicPath directory
+app.use(express.static(publicPath));
 
 // middleware that changes response based on time
 app.use(function(req, res, next) {
@@ -29,5 +41,5 @@ app.use(function(request, response) {
 });
 
 // starts the server
-http.createServer(app).listen(3000);
+app.listen(3000);
 console.log("Server is running at http://localhost:300");
